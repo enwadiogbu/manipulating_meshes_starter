09a20650c8c0a0a321c6277a4fcbf07d9c5506be
enwadiogbu <emekanwadiogbu.jr@me.com>
2025-06-22 13:16:09 -0700

    understanding vector and position property methods within a mesh using standard ThreeJS

--- src/script.js
+++ src/script.js
@@ -6,19 +6,29 @@ const scene = new THREE.Scene();
 
 // add objects to the scene
 const cubeGeometry = new THREE.BoxGeometry(1, 1, 1);
-const cubeMaterial = new THREE.MeshBasicMaterial({ color: "red" });
-
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: "dodgerblue" });
 const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
 scene.add(cubeMesh);
 
+const tempVector = new THREE.Vector3(0,0,0);
+cubeMesh.position.copy(tempVector);
+
+
+const axesHelper = new THREE.AxesHelper(2);
+scene.add(axesHelper);
+
 // initialize the camera
 const camera = new THREE.PerspectiveCamera(
-  35,
+  105,
   window.innerWidth / window.innerHeight,
   0.1,
   200
 );
 camera.position.z = 5;
+
+cubeMesh.position.distanceTo(camera.position);
+
+console.log(cubeMesh.position.distanceTo(camera.position));
 
 // initialize the renderer
 const canvas = document.querySelector("canvas.threejs");
@@ -30,9 +40,15 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 
 // instantiate the controls
-const controls = new OrbitControls(camera, canvas);
+const controls = new OrbitControls('canvas.threejs');
+const renderer = new THREE.WebGLRenderer({
+  canvas: canvas,
+  antialias: true,
+});
+renderer.setSize(window.innerWidth, window.innerHeight);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
 controls.enableDamping = true;
-// controls.autoRotate = true;
+controls.autoRotate = true;
 
 window.addEventListener('resize', () =>{
   camera.aspect = window.innerWidth / window.innerHeight;
